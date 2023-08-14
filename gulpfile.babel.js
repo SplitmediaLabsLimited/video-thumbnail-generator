c1a389221856ddf6741704b285a86d308fb8c6bb
ensu <enrico.suarez@xsplit.com>
2023-08-14 16:27:19 +0800

    chore: update gulpfile to fix dependency update issues

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -21,90 +21,92 @@ paths.buildFiles = [`${paths.buildDir}/**/*.js`];
 /**
  * Task to remove assets from last build
  */
-gulp.task('clean', (done) => {
-  del(['build', 'coverage'], done);
+gulp.task("clean", async (done) => {
+  del(["build", "coverage"]).then(done).catch(util.log);
 });
 
 /**
  * Task to lint the src files
  */
-gulp.task('lint', () => (
-  gulp.src(paths.sourceFiles)
-    .pipe(eslint())
-    .pipe(eslint.failOnError())
-));
+gulp.task("lint", () =>
+  gulp.src(paths.sourceFiles).pipe(eslint()).pipe(eslint.failOnError())
+);
 
 /**
  * Task to run the build
  */
-gulp.task('build', () => (
-  gulp.src(paths.sourceFiles)
+gulp.task("build", () =>
+  gulp
+    .src(paths.sourceFiles)
     .pipe(plumber())
     .pipe(sourcemaps.init())
-    .pipe(babel({
-      presets: ['babel-preset-es2015'],
-    }))
+    .pipe(
+      babel({
+        presets: ["babel-preset-es2015"],
+      })
+    )
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(paths.buildDir))
-    .on('error', util.log)
-));
+    .on("error", util.log)
+);
 
 /**
  * Task to hook anything required before the tests (basically just here for istanbul)
  */
-gulp.task('coverage:instrument', () => (
-  gulp.src(paths.sourceFiles)
-    .pipe(istanbul({
-      instrumenter: Instrumenter,
-      includeUntested: true,
-      exclude: ['src/migrations/**/*.js'],
-    }))
+gulp.task("coverage:instrument", () =>
+  gulp
+    .src(paths.sourceFiles)
+    .pipe(
+      istanbul({
+        instrumenter: Instrumenter,
+        includeUntested: true,
+        exclude: ["src/migrations/**/*.js"],
+      })
+    )
     .pipe(istanbul.hookRequire())
-));
+);
 
 /**
  * Task to conduct the requred tests
  */
-gulp.task('test', () => (
-  gulp.src(paths.tests, { read: false })
-    .pipe(mocha({
-      bail: true,
-      require: 'babel-register',
-      reporter: 'list',
-    }))
-    .on('error', util.log)
-));
+gulp.task("test", () =>
+  gulp
+    .src(paths.tests, { read: false })
+    .pipe(
+      mocha({
+        bail: true,
+        require: "babel-register",
+        reporter: "list",
+      })
+    )
+    .on("error", util.log)
+);
 
-gulp.task('coverage:report', () => (
-  gulp.src(paths.sourceFiles, { read: false })
-    .pipe(istanbul.writeReports({
-      dir: './coverage',
-      reporters: [
-        'lcov',
-        'json',
-        'text',
-        'text-summary',
-      ],
-      reportOpts: {
-        dir: './coverage',
-      },
-    }))
+gulp.task("coverage:report", () =>
+  gulp
+    .src(paths.sourceFiles, { read: false })
+    .pipe(
+      istanbul.writeReports({
+        dir: "./coverage",
+        reporters: ["lcov", "json", "text", "text-summary"],
+        reportOpts: {
+          dir: "./coverage",
+        },
+      })
+    )
     .pipe(istanbul.enforceThresholds({ thresholds: { global: 10 } }))
-    .on('error', util.log)
-));
+    .on("error", util.log)
+);
 
-gulp.task('test:coverage', done => (
-  runSequence('coverage:instrument', 'test', 'coverage:report', done)
-));
+gulp.task("test:coverage", (done) =>
+  runSequence("coverage:instrument", "test", "coverage:report", done)
+);
 
 /**
  * Task to watch the files whilst developing
  */
-gulp.task('watch', () => (
-  gulp.watch(paths.sourceFiles, ['build'])
-));
+gulp.task("watch", () => gulp.watch(paths.sourceFiles, ["build"]));
 
-
-gulp.task('prebuild', ['clean', 'lint', 'test:coverage']);
-gulp.task('default', ['prebuild', 'build']);
-gulp.task('prepublish', ['default']);
+gulp.task("prebuild", gulp.series("clean", "lint", "test:coverage"));
+gulp.task("default", gulp.series("prebuild", "build"));
+gulp.task("prepublish", gulp.series("default"));
